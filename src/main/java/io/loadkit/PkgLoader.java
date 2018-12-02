7703d299651bed8bd03f3c3b7d8a54352211b595
core-lib <646742615@qq.com>
2018-12-02 13:34:48 +0800

    1. 实现正则表达式风格路径表达式资源加载器

---
+++ src/main/java/io/loadkit/PkgLoader.java
@@ -0,0 +1,30 @@
+package io.loadkit;
+
+import java.io.IOException;
+import java.util.Enumeration;
+
+/**
+ * 包名表达式资源加载器
+ *
+ * @author Payne 646742615@qq.com
+ * 2018/12/2 13:31
+ */
+public class PkgLoader extends DelegateLoader implements Loader {
+
+    public PkgLoader() {
+        this(new ProjLoader());
+    }
+
+    public PkgLoader(ClassLoader classLoader) {
+        this(new ProjLoader(classLoader));
+    }
+
+    public PkgLoader(Loader delegate) {
+        super(delegate);
+    }
+
+    public Enumeration<Resource> load(String pkg, boolean recursively, Filter filter) throws IOException {
+        String path = pkg.replace('.', '/');
+        return delegate.load(path, recursively, filter);
+    }
+}
